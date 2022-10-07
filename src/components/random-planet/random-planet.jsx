a6c890c0efb472ee582da558547fe8d3a4cb96a3
rdanov <rdanov80@gmail.com>
2022-10-07 16:35:04 +0300

    creating a function that transforms data from api

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -2,27 +2,53 @@ import React from "react";
 
 import "./random-planet.css";
 
+import SwapiService from "../../services/swapi-service";
+
 class RandomPlanet extends React.Component {
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 15 + 2);
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+  }
+
   render() {
+    const {
+      planet: { population, rotationPeriod, diameter, name, id },
+    } = this.state;
+
     return (
       <div className="random-planet jumbotron rounded">
         <img
           className="planet-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
         />
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <span className="term">Population</span>
+              <span className="term">{population}</span>
               <span>123124</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Rotation Period</span>
+              <span className="term">{rotationPeriod}</span>
               <span>43</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Diameter</span>
+              <span className="term">{diameter}</span>
               <span>100</span>
             </li>
           </ul>
